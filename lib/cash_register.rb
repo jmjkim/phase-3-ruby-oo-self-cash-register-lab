0c66e1411644b8277be6d542fe3533dfaef08ca6
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-06-24 18:57:42 -0700

    Completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+    attr_reader :items
+    attr_accessor :total, :discount, :price, :quantity, :last_transaction_total
+    
+    def initialize(discount = 0)
+        @items = []
+
+        @total = 0
+        self.discount = discount
+    end
+    
+    def add_item(title, price, quantity = 1)
+        self.price = price
+        self.quantity = quantity
+
+        @total += price * quantity
+
+        quantity.times { @items << title }
+    end
+
+    def apply_discount
+        if (self.discount != 20)
+            "There is no discount to apply."
+        else
+            @total -= (@total * self.discount / 100)
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def items
+        return @items
+    end
+
+    def void_last_transaction
+        @total -= self.price * self.quantity
+    end
+end
